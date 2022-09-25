a5898f1f4dbaffd7b687de4b0e9e516fac43e313
mostly-harmless42 <wrgread1@gmail.com>
2022-09-25 10:58:21 -0700

    Edit knight-travail.rb to lessen sloc

--- knight-travail.rb
+++ knight-travail.rb
@@ -24,14 +24,12 @@ class Square
     @prev = prev
   end
 
-
   def move_list()
     (0..7).map { |i| [2 ** (i % 2) * ((-1) ** (i / 4)), 2 ** ((i + 1) % 2) * ((-1) ** (i / 2 % 2))] }
           .map { |a, b| [@coord[0] + a, @coord[1] + b] }
           .select { |a, b| a >= 0 and a < 8 and b >= 0 and b < 8 }
           .map { |coord| Square.new(coord, self) }
   end
-
 
   def path_list
     path = [self]
